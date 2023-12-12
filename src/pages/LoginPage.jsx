d9476eb06b05a885c22c067db8dda816592779ac
jennlai95 <jenniferlaai@gmail.com>
2023-12-12 22:59:50 +1100

    Login page functions

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 
 // login form 
 export default function LoginPage ()  {
-return (
+
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  async function handleLoginSubmit(ev) {
+    ev.preventDefault();
+    try {
+      await  axios.post('/login', {
+        email,
+        username,
+        password,
+      });
+      alert('Login succesful')
+    } catch (e) {
+      alert('Error, please try again')
+    }
+  }
+
+  return (
     <div className='mt-4 grow flex items-center justify-around'>
       <div className='mb-64'>
-        <h1 className='text-4xl text-center'> Login form </h1>
+        <h1 className='text-4xl text-center'> Login  </h1>
         <form className='max-width-md mx-auto '>
           <input type="email" placeholder='your@email.com'/>
           <input type="username" placeholder='username'/>
