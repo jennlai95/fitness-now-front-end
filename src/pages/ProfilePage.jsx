9fe6c8052d4209b987d44a578acb9d9284ff3a71
jennlai95 <jenniferlaai@gmail.com>
2023-12-17 21:45:14 +1100

    placeholder admin and profile pages

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import Footer from '../components/Footer';
+
 
 function Profile() {
 
-  const { username } = useParams();
+  const { id } = useParams();
 
   // establish profile detail state 
   const [profileData, setProfileData ] = useState({
@@ -14,34 +16,38 @@ function Profile() {
   }
   );
 
-  const API_URL= 'https://fitness-studio-app-e1ebefd30762.herokuapp.com/users/one/id${id}'
+  const API_URL= 'https://fitness-studio-app-e1ebefd30762.herokuapp.com/'
 
   // fetch user details by username /id 
   useEffect(() => {
     const fetchData = async () => {
-  
-        const response = await axios.get(API_URL)
-        const json = await response.json();
-        setProfileData(json); // Updates the user state with the fetched data.
-
-     };
-     fetchData();
-    }, []);
-
+      try {
+        const response = await axios.get(`${API_URL}users/one/id/${id}`);
+        const data = response.data;
+        setProfileData(data); // Updates the user state with the fetched data.
+      } catch (error) {
+        console.error('Error fetching profile data:', error);
+      }
+    };
+    fetchData();
+  }, [id]);
   
 
   return (
     <div>
       <Layout/>
-      Profile
-      <Layout/>
-      <div class="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-3 h-screen"> 
-      <div> User information 
+      <h1 className='text-center bg-sky-100'>Profile Page </h1>
+      <div class="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-2 lg:grid-cols-2 h-screen"> 
+      <div className='bg-blue-50'> User information 
         <p> Username : {profileData.username}</p>
         <p> Email : {profileData.email}</p>
+        <button> Update/Delete User </button> 
       </div>
-      <div> Booking History </div>
+      <div className='bg-blue-50'> <h1 className='text-center'>Booking History</h1> 
+      <p> placeholder </p>
+        <button> Edit Booking </button> </div>
       </div>
+      <Footer/>
       </div> 
   )
 }
