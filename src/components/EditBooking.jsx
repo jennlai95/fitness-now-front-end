8d6a84e1531c626f7deaae43bb42c12aea42f054
jennlai95 <jenniferlaai@gmail.com>
2023-12-19 22:10:54 +1100

    updated styling to be more responsive

--- src/components/EditBooking.jsx
+++ src/components/EditBooking.jsx
@@ -94,9 +94,9 @@ const EditBooking = () => {
             
             <h1 className='text-3xl my-4 text-center'>Edit Booking</h1>
  
-            <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+            <div className='flex flex-col border-2 border-sky-400 rounded-xl max-w-screen-md p-4 mx-auto'>
                 <div className='my-4'>
-                <label className='text-xl mr-4 text-gray-500'>Title</label>
+                <label className='text-xl mr-4 text-gray-500'>Class </label>
                 <input
                     type='text'
                     value={title}
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -36,13 +36,13 @@ function AdminPortal() {
       <div>
         <Layout/>
         <h1 className='text-center bg-sky-100'>Profile Page </h1>
-        <div class="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-2 lg:grid-cols-2 h-screen"> 
-        <div className='bg-blue-50'> User information 
+        <div class="mt-8 grid gap-x-6 gap-y-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 h-screen"> 
+        <div className='bg-blue-50 p-4'> User information 
           <p> Username : {profileData.username}</p>
           <p> Email : {profileData.email}</p>
           <button> Update/Delete User </button> 
         </div>
-        <div className='bg-blue-50'> <h1 className='text-center'>Booking History</h1> 
+        <div className='bg-blue-50 p-4'> <h1 className='text-center'>Booking History</h1> 
         <p> placeholder </p>
           <EditBooking/> </div>
         </div>
