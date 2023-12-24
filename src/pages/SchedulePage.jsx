b0e708e35a41fa28254db72e7e8eef568363d634
jennlai95 <126547898+jennlai95@users.noreply.github.com>
2023-12-24 13:16:22 +0800

    updated date picker to look for schedule on date

--- src/pages/SchedulePage.jsx
+++ src/pages/SchedulePage.jsx
@@ -8,24 +8,42 @@ import DatePicker from 'react-datepicker';
 
 function Schedule() {
   // State to store the schedule data
-  const [scheduleData, setScheduleData] = useState([]);
+  const [allSchedules, setAllSchedules] = useState([]);
+
+  // state to store schedule data on selected date data
   const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedDateSchedules, setSelectedDateSchedules] = useState([]);
   
 
   // Fetch schedule data from the backend when the component mounts
   useEffect(() => {
-    fetchScheduleData();
-  }, []);
+    fetchAllSchedules();
+    if (selectedDate) {
+      fetchScheduleData(selectedDate);
+    }
+  }, [selectedDate]);
 
   // Function to fetch schedule data from the backend
-  const fetchScheduleData = async () => {
+  const fetchAllSchedules = async () => {
     try {
       const response = await fetch('https://fitness-studio-app-e1ebefd30762.herokuapp.com/schedule/all'); 
       const data = await response.json();
-      setScheduleData(data.result);
+      setAllSchedules(data.result);
       // error handling
     } catch (error) {
       console.error('Error fetching schedule data:', error);
+    }
+  };
+
+  // Function to fetch schedules for the selected date from the backend
+  const fetchScheduleData = async (date) => {
+    try {
+      const formattedDate = date.toISOString().split('T')[0];
+      const response = await fetch(`https://fitness-studio-app-e1ebefd30762.herokuapp.com/schedule/date/${formattedDate}`);
+      const data = await response.json();
+      setSelectedDateSchedules(data.result);
+    } catch (error) {
+      console.error('Error fetching schedule data for the selected date:', error);
     }
   };
 
@@ -44,9 +62,10 @@ function Schedule() {
                   dateFormat="dd/MM/yyyy"
                 />
               </div>
-              
+
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-              {scheduleData.map((schedule) => (
+              {/* display selected date schedule */}
+              {selectedDateSchedules.map((schedule) => (
                 <div key={schedule._id} className='bg-white p-4 rounded-md shadow-md'>
                   <img src={pilates} class="mx-auto  " alt="reformer"/>
                   <div className='text-center'>
@@ -61,6 +80,23 @@ function Schedule() {
                   </div>
                 </div>
               ))}
+
+                {/* Display all schedules */}
+                {allSchedules.map((schedule) => (
+                <div key={schedule._id} className='bg-white p-4 rounded-md shadow-md'>
+                <img src={pilates} className='mx-auto' alt='reformer' />
+                <div className='text-center'>
+                  <p className='text-sm text-gray-500'>Class: {schedule._name}</p>
+                  <p className='text-sm text-gray-500'>Date: {new Date(schedule.date).toLocaleDateString()}</p>
+                  <p className='text-sm text-gray-500'>Time: {schedule.time.start} - {schedule.time.end}</p>
+                  <Link to={'/booking'}>
+                    <button className='mt-2 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600'>
+                      Book Now
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            ))}
             </div>
           </div>
           <Footer/>
